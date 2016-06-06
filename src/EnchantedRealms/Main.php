dda9b3bb3e032fce08f64da6b5495c3161362724
Joe [ERPE] [SA] <JoeycraftPE@gmail.com>
2016-06-06 11:54:24 -0500

    Create Main.php

---
+++ src/EnchantedRealms/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace EnchantedRealms;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\Item;
+
+class Listener implements Listener {
+
+    public function onDeath(PlayerDeathEvent $event) {
+        $cause = $event->getEntity()->getLastDamageCause()->getCause();
+        if($cause instanceof EntityDamageByEntityEvent) {
+            $event->setDrops([Item::get(Item::GOLDEN_APPLE, 0, 1)]);
+            $event->setDrops([Item::get(Item::ENCHANTING_BOTTLE, 0, 10)]);
+            
+        }
+        
+    }
+    
+}
